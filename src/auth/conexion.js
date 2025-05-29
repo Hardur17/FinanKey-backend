ddd3c02e0add714f4607fbc96c767afd09129a5e
Hardur17 <harolduran2005@gmail.com>
2025-05-29 00:06:21 -0500

    Corregido conexion.js para usar process.env en Render

--- src/auth/conexion.js
+++ src/auth/conexion.js
@@ -1,20 +1,18 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
-dotenv.config();
-const fs = require('fs');
-console.log("¿Existe .env?", fs.existsSync(".env"));
-console.log("DB_NAME =", process.env.DB_NAME);
-
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,  
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,  
     database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
-    if (err) throw err;
+    if (err) {
+        console.error('Error de conexión:', err);
+        return;
+    }
     console.log('Conexión a la base de datos establecida');
 });
 
