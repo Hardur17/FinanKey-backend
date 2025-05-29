f280e0bfbc7974c3733267df316b677caf867f9b
Hardur17 <harolduran2005@gmail.com>
2025-05-29 00:00:35 -0500

    Eliminado test-ip.js para producción

--- src/test-ip.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end(`IP pública del servidor: ${req.connection.remoteAddress}`);
-}).listen(3000, () => {
-  console.log('Servidor escuchando en puerto 3000');
-});
